e214d97074908ccf761bee77f30971384a0f7faa
torbenkrah2006 <92218280+torbenkrah2006@users.noreply.github.com>
2021-10-09 10:11:03 -0400

    Create passwordgenV1.py

---
+++ passwordgenV1.py
@@ -0,0 +1,15 @@
+import random
+
+chars = 'ABCDEFGHIJKLOMPQRSTUVWXYZabcdefghijklomnpqrstuvwxyz1234567890~!@#$%^&*()_+?<>-'
+
+length = input('gennerated password length:')
+length = int(length)
+
+passwords = input('amount of passwords to geenerate:')
+passwords = int(passwords)
+
+for p in range(passwords):
+  password = ''
+  for c in range(length):
+    password += random.choice(chars)
+  print(password)
